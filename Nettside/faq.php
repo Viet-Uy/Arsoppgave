bb7ab2c75f78d8cec9ee0955a44a26abea13814b
navn etternavn <navn@email.com>
2023-05-31 11:48:17 +0200

    OppdatertFAQ

--- Nettside/faq.php
+++ Nettside/faq.php
@@ -18,62 +18,65 @@
 <h2>
 
 1. Hvordan starter jeg spillet?<br>
-    - sadasd <br>
+    - Du starter spillet ved å trykke på Enter og skrive inn navnet du ønsker å spille som <br>
              <br>
     
 2. Hvilket plattformer er spillet tilgjengelig for?<br>
-    - sadasd <br>
+    - For nå er det bare tilgjenglig på pc, på alle OS som kan installere spillet fra Github <br>
              <br>
 3. Hva er målet med spillet?<br>
-    - sadasd <br>
+    - Målet med spillet er å undervise brukeren om the butterfly effect og hva slags effekt den har, gjennom artig spill med humor og konkuranse <br>
              <br>
 4. Hvordan kontrollerer jeg spillerfiguren i spillet?<br>
-    - sadasd <br>
+    - Måten du kontorllerer spillerfiguren er ved å trykke på blant 4 ulike bokser som gir 4 ulike valg. <br>
              <br>
 
 5. Hvor kan jeg finne instruksjoner eller veiledning for spillet?<br>
-    - sadasd <br>
+    - På nettsiden vår ovenfor så ser du en veiledning for spillet og hvordan du starter kjappt med spillet :) <br>
              <br>
 
 6. Kan jeg spille spillet uten internettforbindelse?<br>
-    - sadasd <br>
+    - Spillet trenger en internettforbindelse for å klare å save progresjonen, om du ikke syns det er nødvendig så er det bare å spille i vei. <br>
              <br>
 
-7. Hva er forskjellen mellom singleplayer- og multiplayer-modus?<br>
-    - sadasd <br>
+7. Hvordan lagrer jeg spillet mitt?<br>
+    - På høyrehjørne så ser du en save knapp, når du har savet så skal knappen forandre seg til "Saved" <br>
+             <br>
+8. Hvordan kan jeg kontakte kundestøtte hvis jeg opplever problemer med spillet?<br>
+    - Per nå må du kontakte via mailen vår som står nederst i siden, svar tiden kan variere fra 1 til 3 virkedager <br>
              <br>
 
-8. Hvordan lagrer jeg spillet mitt?<br>
-    - sadasd <br>
-             <br>
-9. Hvordan kan jeg kontakte kundestøtte hvis jeg opplever problemer med spillet?<br>
-    - sadasd <br>
+9. Er spillet mitt kompatibelt med ulike skjermoppløsninger?<br>
+    - Spillet er i fast skjermoppløsning men mesteparten av de moderne skjermene i dag er innenfor grense for å spille til ekstent <br>
              <br>
 
-10. Er spillet mitt kompatibelt med ulike skjermoppløsninger?<br>
-    - sadasd <br>
+10. Er det mulig å endre språket i spillet?<br>
+
+    - Per nå er det ikke mulig å endre språket, du må spille på engelsk<br>
              <br>
 
-11. Er det mulig å endre språket i spillet?<br>
-
-    - sadasd <br>
-             <br>
-12. Er det noen aldersbegrensninger for å spille spillet?<br>
-    - sadasd <br>
+11. Er det noen aldersbegrensninger for å spille spillet?<br>
+    - Aldersgrense er 16+ da det er vold i spillet <br>
              <br>
 
-13. Kan jeg tilpasse lyd- og grafikkinnstillingene i spillet?<br>
-    - sadasd <br>
+12. Kan jeg tilpasse lyd- og grafikkinnstillingene i spillet?<br>
+    - Det er ikke noe lyd eller grafikk i spillet som kan forandres eller trengs <br>
              <br>
 
-14. Er det noen planer om å legge til nye oppdateringer eller utvidelser i fremtiden?<br>
-    - sadasd <br>
+13. Er det noen planer om å legge til nye oppdateringer eller utvidelser i fremtiden?<br>
+    - I framtiden tenker vi å utvikle det sånn at alle kan nyte spillet vårt, vi prøver dermed å følge universell utforming sånn at hver eneste person, uansett om de har forskjellige funksjonshemmninger skal klare å nyte spille til sitt fulleste. <br>
              <br>
 
-15. Hvordan fungerer spillets poengsystem?<br>
-    - sadasd <br>
+14. Hvordan fungerer spillets poengsystem?<br>
+    - Poengsystemet er hvor langt du har kommet, hva slags våpen du har og hvor mye hp du har. Desto mer desto sterkere :) <br>
              <br>
 
+
+</h2>
+<br>
+<br>
+<h2>
+Contact customer service at: Epicgame@hotmail.com
 
 </h2>
 
